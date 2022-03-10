8a2af229d4f13c199a7a038d8b4ef46bf563d85e
Андрей Скоробогатов <ufo1og@yandex.ru>
2022-03-10 16:41:51 +0500

    added quickSort and quickSortThreeRanges methods in ArrayManager

--- src/ArrayManager.java
+++ src/ArrayManager.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -6,7 +7,7 @@ public class ArrayManager {
 
     private static void fillArray(int maxValue, int[] array) {
         for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(maxValue);
+            array[i] = random.nextInt(maxValue + 1);
         }
     }
 
@@ -53,4 +54,58 @@ public class ArrayManager {
         int[] resultArray = Arrays.copyOf(array, array.length - shift);
         System.out.println(Arrays.toString(resultArray));
     }
+
+    public static void quickSortThreeRanges (int[] a, int l, int r) {
+        if ((r - l) <= 0) return;
+        int i = l;
+        int j = r;
+        int p = (l + r) / 2;
+        int q = p;
+        int v = a[p];
+        while (i <= j) {
+            while (a[i] < v) {
+                i++;
+            }
+
+            while (a[j] > v) {
+                j--;
+            }
+            if (i >= p && j <= q) break;
+            swapElements(a, i, j);
+            if (a[i] == v) {
+                if (i >= p) {
+                    if (a[i] != a[p]) swapElements(a, i, p = i + 1);
+                }
+                else swapElements(a, i, --p);
+            }
+            if (a[j] == v) {
+                if (j <= q)  {
+                    if (a[i] != a[p]) swapElements(a, j, q = j - 1);
+                }
+                else swapElements(a, j, ++q);
+            }
+        }
+        p = Integer.max(i, p) - 1;
+        q = Integer.min(j, q) + 1;
+        quickSortThreeRanges(a, l, p );
+        quickSortThreeRanges(a, q, r);
+    }
+
+    public static void quickSort (int[] a, int l, int r) {
+        if ((r - l) < 1) return;
+        int i = l;
+        int j = r;
+        int v = a[(l + r) / 2];
+        while (i <= j) {
+            while(a[i] < v) i++;
+            while(a[j] > v) j--;
+            if (i >= j) break;
+            swapElements(a, i++, j--);
+        }
+        //System.out.println(i + " " + j);
+        if (i == j) quickSort(a, i + 1, r);
+        else quickSort(a, i, r);
+        quickSort(a, l, j);
+    }
+
 }
